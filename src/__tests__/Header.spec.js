126eb40437952a2a090c6a90eab9ec1525847192
MoniqueOg <moniqueogunk@icloud.com>
2023-01-07 14:33:04 +0000

    feat added spec element

--- src/__tests__/Header.spec.js
+++ src/__tests__/Header.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Header from '../components/Header';
@@ -9,9 +9,9 @@ describe("Header functionality", () => {
     const count = 0;
     render(<BrowserRouter>
     <Routes>
-      <Route exact path="/" render={() => (
-        <Header basketCount={count} />
-      )} />
+      <Route exact path="/" element= {
+        <Header basketCount={count} />}/>
+      
       </Routes>
     </BrowserRouter>);
     expect(document.querySelectorAll("a").length).toBeGreaterThanOrEqual(3);
